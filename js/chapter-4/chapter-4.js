f3aca7434b84cfc2b9ad9fe389c13104b4e6bc45
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-18 10:02:51 -0600

    Reverse array and sum of range completed

--- js/chapter-4/chapter-4.js
+++ js/chapter-4/chapter-4.js
@@ -23,15 +23,56 @@ console.log(obj4);
 console.log('------------------THE SUM OF A RANGE-------------------------');
 // THE SUM OF A RANGE
 // Write a range function that takes two arguments, start and end, and returns an array containing all the numbers, inclusive.
-// Write a sum function that takes an array of numbers and returns their sum
 // Bonus: modify range() to take an optional third argument that indicates the 'step' value used when building the array. If no argument is given, default to '1'
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+let range = function (start, end, step = 1) {
+    let rangeArray = [];
+    while (start <= end) {
+        rangeArray.push(start);
+        start += step;
+    }
+    return rangeArray;
+}
+console.log(`Range: ${range(1, 10, 2)}`);
+// Write a sum function that takes an array of numbers and returns their sum
+
+let sum = function (numArray) {
+    let add = 0;
+    for (let num of numArray) {
+        add += num;
+    }
+    return add;
+}
+console.log(`Sum: ${sum(testArray)}`);
+
+console.log(`Sum of Range: ${sum(range(1, 10, 2))}`);
 
 // console.log(sum(range(1, 10)));
 console.log('------------------REVERSING AN ARRAY-------------------------');
 // REVERSING AN ARRAY
 // Write reverseArray, that takes an array as argument and produces a new array that has the same elements in the inverse order.
+let reverseArray = function (arr) {
+    let reversedArray = [];
+    for (let element of arr) {
+        // I am not sure why, but using reversedArray.push(arr.pop()) would return a reversed array of half the length...
+        reversedArray.unshift(element);
+    }
+    return reversedArray;
+}
+console.log(reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
 // Write reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+// You have to be careful not to overwrite elements that you will later need. The trick is to swap the first and last elements, then the second and second-to-last, and so on. You can do this by looping over half the length of the array (use Math.floor to round down—you don’t need to touch the middle element in an array with an odd number of elements) and swapping the element at position i with the one at position array.length - 1 - i. You can use a local binding to briefly hold on to one of the elements, overwrite that one with its mirror image, and then put the value from the local binding in the place where the mirror image used to be.
+// Full disclosure, this is the book solution as I couldn't figure it out on my own:
+let reverseArrayInPlace = function (arr) {
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        let original = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = original;
+    }
+    return arr;
+}
+console.log(reverseArrayInPlace(testArray));
 // Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
 
 console.log('------------------A LIST-------------------------');
