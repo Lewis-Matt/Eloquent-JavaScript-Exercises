416dfba3cbf4635680f209a15ac3f9a746cc7a10
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-09 16:39:08 -0600

    finished flattening

--- js/chapter-5/chapter-5.js
+++ js/chapter-5/chapter-5.js
@@ -201,12 +201,21 @@ console.log('--------------EXERCISE: FLATTENING---------------')
 // → [1, 2, 3, 4, 5, 6]
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+function flatten(arr) {
+    return arr.reduce((finalArr, current) => {
+        return finalArr.concat(current)
+    })
+}
+console.log(flatten(arrays));
+
+
 console.log('--------------EXERCISE: YOUR OWN LOOP---------------')
 // TODO: Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
 
 // Your code here.
 
-loop(3, n => n > 0, n => n - 1, console.log);
+// loop(3, n => n > 0, n => n - 1, console.log);
 // // → 3
 // // → 2
 // // → 1
