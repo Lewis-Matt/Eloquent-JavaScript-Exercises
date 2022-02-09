2fed035451031a2e01083a9f4260fe444d569bfd
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-09 16:49:08 -0600

    finished loop

--- js/chapter-5/chapter-5.js
+++ js/chapter-5/chapter-5.js
@@ -207,15 +207,19 @@ function flatten(arr) {
         return finalArr.concat(current)
     })
 }
+
 console.log(flatten(arrays));
 
-
 console.log('--------------EXERCISE: YOUR OWN LOOP---------------')
+
 // TODO: Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
+function loop(value, testFN, updateFN, bodyFN) {
+    for (let iterator = value; testFN(iterator); iterator = updateFN(iterator)) {
+        bodyFN(iterator);
+    }
+}
 
-// Your code here.
-
-// loop(3, n => n > 0, n => n - 1, console.log);
+loop(3, n => n > 0, n => n - 1, console.log);
 // // → 3
 // // → 2
 // // → 1
